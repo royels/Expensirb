aa979d6271f53ef8c2369a4b331703f5582b777b
royels <docmentz@gmail.com>
2016-02-08 20:34:35 -0800

    I reorg'd again, probably not going to get to writing it fully till Thurs

--- lib/Expensirb.rb
+++ lib/Expensirb.rb
@@ -5,10 +5,22 @@ require 'uri'
 require '../lib/expensirb/errors/error'
 
 module Expensirb
+  class << self
+    attr_accessor :api_key, :protocol, :api_url
+
+  end
+
+  def self.submit(method, url, options={})
+
+  end
 
 
 
+  def Expensirb(options={})
+    options[:api_key] ||= Expensirb.api_key || ""
+    options[:api_url] ||= Expensirb.api_url || "https://integrations.expensify.com/Integration-Server/ExpensifyIntegrations"
+  end
 
 
-
+  return Expensirb::Handler.new(options)
 end
