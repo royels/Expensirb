198303e5df5ebe1489a4e8f5d050b3873b433a3d
royels <docmentz@gmail.com>
2016-02-13 20:00:16 -0800

    I wonder if Trader Joe's is open

--- lib/expensirb/v1/employee.rb
+++ lib/expensirb/v1/employee.rb
@@ -1,5 +1,28 @@
 
 module Expensirb
   class Employee
+
+
+    def update(method, opts={})
+      requestJobDescription = {}
+      requestJobDescription[:type] = "create"
+      requestJobDescription[:credentials] = Expensirb::Constants::CREDENTIALS
+
+      inputSettings = {}
+      inputSettings[:type] = "employees"
+      inputSettings[:fileType] = "csv"
+      inputSettings[:policyID] = opts[:policyID]
+      requestJobDescription[:inputSettings] = inputSettings
+
+      final_json = requestJobDescription.to_json
+
+      cols = opts[:column_headers]
+      data << cols.join(',')
+      opts[:data].each do |i|
+        data << i.join(',')
+      end
+      Expensirb.make_request method, Expensirb::Constants::API_URL, parameters: [final_json, data]
+
+    end
   end
 end
